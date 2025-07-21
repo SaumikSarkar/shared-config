f9511ebd5717323cac52ac0a994e74533895093f
Saumik Sarkar <sarkar.saumik15@gmail.com>
2025-07-21 22:22:20 +0530

    fix(eslint-config): remove comment to trigger auto-release
    Removes comment to trigger auto-release for testing semantic-release
    
    Fixes: SSPORT-16

--- packages/eslint-config/presets/vitest-react.js
+++ packages/eslint-config/presets/vitest-react.js
@@ -2,7 +2,6 @@ module.exports = {
 	plugins: ["testing-library"],
 	extends: ["./vitest", "./react", "plugin:testing-library/react"],
 	rules: {
-		// Same testing-library rules as jest-react
 		...require("./jest-react").rules,
 	},
 };
